bb276e3db17a600549a351043f58dbb4f823acf9
DARKSIDEOPG <28642811+DARKSIDEOPG@users.noreply.github.com>
2021-03-25 17:36:59 +0300

    Add files via upload

---
+++ MEBFilePatcher/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace MEBFilePatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                throw new ArgumentException("Please provide a path to the input file.");
+            }
+
+            if (args[0].Contains("_patch"))
+            {
+                throw new Exception("Already patched.");
+            }
+
+            using (FileStream fstream = new FileStream(args[0], FileMode.Open))
+            {
+                Console.WriteLine("Reading input stream from " + args[0]);
+
+                // Start reading from 12th byte
+                fstream.Seek(12, SeekOrigin.Begin);
+
+                // Code from MS Docs :)
+                byte[] input = new byte[fstream.Length];
+                int numBytesToRead = (int)fstream.Length - 12;
+                int numBytesRead = 0;
+                while (numBytesToRead > 0)
+                {
+                    // Read may return anything from 0 to numBytesToRead.
+                    int n = fstream.Read(input, numBytesRead, numBytesToRead);
+
+                    // Break when the end of the file is reached.
+                    if (n == 0)
+                        break;
+
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+                }
+                numBytesToRead = input.Length;
+
+                // Write the byte array to the other FileStream.
+                using (FileStream fsNew = new FileStream(Path.GetFileNameWithoutExtension(args[0]) + "_patch.meb",
+                    FileMode.Create, FileAccess.Write))
+                {
+                    // Add a header
+                    byte[] header = new byte[] { 00, 00, 00, 01, 00, 01, 00, 00 };
+
+                    // Combine with file data
+                    byte[] output = header.Concat(input).ToArray();
+
+                    // Write to the output file
+                    Console.WriteLine("Writing output...");
+                    fsNew.Write(output, 0, numBytesToRead);
+                }
+            }
+            Console.WriteLine("Done.");
+        }
+    }
+}
+
---
+++ TEXFilePatcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("TEXFilePatcher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TEXFilePatcher")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("399aa5ed-8ad1-4160-b47e-9efe8f76ed16")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
